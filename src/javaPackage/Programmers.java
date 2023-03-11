7a4d7a98b67b9361ab7db0b0c48307ccdd0aa45b
윤건수 <ygs3004@naver.com>
2023-03-12 02:10:51 +0900

    프로그래머스 0레벨 다음에 올 숫자

---
+++ src/javaPackage/Programmers.java
@@ -0,0 +1,11 @@
+package javaPackage;
+
+public abstract class Programmers {
+
+    public abstract int solution(int[] parameter);
+
+    public void result(int[] parameter){
+        System.out.println(solution(parameter));
+    }
+
+}
