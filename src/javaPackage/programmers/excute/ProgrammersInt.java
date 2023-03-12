82717b56876fc6bbe98ac6b7f5db47a890b3d252
윤건수 <ygs3004@naver.com>
2023-03-13 01:13:57 +0900

    프로그래머스 0레벨 몇문제

--- src/javaPackage/programmers/excute/ProgrammersInt.java
+++ src/javaPackage/programmers/excute/ProgrammersInt.java
@@ -10,6 +10,10 @@ public abstract class ProgrammersInt {
     public int solution(int[] a, int b){
         return 0;
     }
+
+    public int solution(int a){
+        return 0;
+    }
     public int solution(int a, int b){
         return 0;
     }
@@ -23,6 +27,11 @@ public abstract class ProgrammersInt {
         System.out.println("parameter 1 : "+ Arrays.toString(a));
         System.out.println("parameter 2 : "+ b);
         System.out.println("result : " + solution(a, b));
+    }
+
+    public void result(int a) {
+        System.out.println("parameter 1 : "+ a);
+        System.out.println("result : " + solution(a));
     }
 
     public void result(int a, int b) {
