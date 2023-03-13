0a0cc64451ba446f70ae497cf003bc5b07210d38
윤건수 <ygs3004@naver.com>
2023-03-13 09:29:35 +0900

    프로그래머스 0레벨

--- src/nodejs/programmers/level_0/calc-result.js
+++ src/nodejs/programmers/level_0/calc-result.js
@@ -1,3 +1,10 @@
+/*
+정수 num1, num2가 매개변수로 주어질 때, num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+num1	num2	result
+10	5	2
+7	2	3
+*/
+
 function solution(num1, num2) {
     let answer = 0;
 
@@ -5,3 +12,6 @@ function solution(num1, num2) {
 
     return answer;
 }
+
+console.log(solution(10, 5));
+console.log(solution(7, 2));
