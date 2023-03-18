aaa888b428a93ee6b1ed6f2717f540cb2704f8d5
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-03-19 02:42:16 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- src/programmers/lv0/lv0_120850/Solution.js
+++ src/programmers/lv0/lv0_120850/Solution.js
@@ -1,3 +1,5 @@
 function solution(my_string) {
-    return [...my_string].filter( v => v == 0 || parseInt(v)).map(v => v == 0 ? 0 : +v).sort();
+    
+    return [...my_string].filter( v => v == 0 || parseInt(v)).map(v => +v).sort();
+    
 }
