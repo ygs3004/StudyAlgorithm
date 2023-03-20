f28f69978ed0041868d09dca122ec8ba031c3876
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-03-21 00:13:34 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 2.65 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ src/programmers/lv0/lv0_120864/Solution.java
@@ -0,0 +1,32 @@
+package programmers.lv0.lv0_120864;
+
+class Solution {
+    
+    public int solution(String my_string) {
+
+        System.out.println((int)'0'); // 48
+        System.out.println((int)'9'); // 57
+        int sum = 0;
+        String number = "";
+        
+        for(int i=0; i<my_string.length(); i++){
+            
+            if(my_string.charAt(i)>= 48 && my_string.charAt(i) <= 57){
+                number += Character.toString(my_string.charAt(i));
+                System.out.println("number: " + number);
+            }else if(number.length()>0){
+                System.out.println("number: " + number);
+                sum += Integer.parseInt(number);
+                System.out.println("sum: " + number);
+                number = "";
+            }
+            
+        }
+        
+        if(number.length() > 0) sum += Integer.parseInt(number);
+        
+         return sum;
+        
+    }
+    
+}
