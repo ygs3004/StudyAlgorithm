bd8036e11174108c430034afff381b9d87669e64
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-03-22 01:09:36 +0900

    [level 0] Title: 나머지 구하기, Time: 0.02 ms, Memory: 66.3 MB -BaekjoonHub

---
+++ src/programmers/lv0/lv0_120810/Solution.java
@@ -0,0 +1,10 @@
+package programmers.lv0.lv0_120810;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        int answer = num1%num2;
+        
+        return answer;
+    }
+}
