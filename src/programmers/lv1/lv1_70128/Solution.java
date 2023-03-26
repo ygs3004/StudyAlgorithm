26cf21efcc1d35243722c27619e86d465576556f
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-03-26 16:31:12 +0900

    [level 1] Title: 내적, Time: 0.03 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ src/programmers/lv1/lv1_70128/Solution.java
@@ -0,0 +1,15 @@
+package programmers.lv1.lv1_70128;
+
+class Solution {
+    
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        
+        for(int i=0; i<a.length; i++){
+            answer += a[i]*b[i];
+        }
+        
+        return answer;
+    }
+    
+}
