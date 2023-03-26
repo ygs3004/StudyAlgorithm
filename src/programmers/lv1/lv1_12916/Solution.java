e2317edc1f2782171aa5fdff3b3d90e2771a4da9
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-03-26 11:34:24 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.98 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ src/programmers/lv1/lv1_12916/Solution.java
@@ -0,0 +1,20 @@
+package programmers.lv1.lv1_12916;
+
+class Solution {
+    
+    boolean solution(String s) {
+        
+        boolean answer = true;
+        int check = 0;
+        
+        char[] sArr = s.toLowerCase().toCharArray();
+        
+        for(char a : sArr){
+            check = a == 'p' ? check+1 : a == 'y' ? check-1 : check;
+            System.out.println(check);
+        }
+        
+        return check == 0 ? true : false;
+    }
+    
+}
