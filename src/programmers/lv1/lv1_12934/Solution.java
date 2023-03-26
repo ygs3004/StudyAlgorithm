9f327540b745876e9f8d805b87f7be476bd813b4
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-03-26 11:22:52 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 77.7 MB -BaekjoonHub

--- src/programmers/lv1/lv1_12934/Solution.java
+++ src/programmers/lv1/lv1_12934/Solution.java
@@ -2,6 +2,6 @@ package programmers.lv1.lv1_12934;
 
 class Solution {
     public long solution(long n) {
-        return Math.pow((int)Math.sqrt(n), 2) == n ? (long)Math.pow((int)Math.sqrt(n)+1, 2) : -1;
+        return Math.pow((int)Math.sqrt(n), 2) == n ? (long)Math.pow(Math.sqrt(n)+1, 2) : -1;
     }
 }
