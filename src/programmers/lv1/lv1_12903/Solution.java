e55d7757746f0d05a3fc0f4cbc2c3d3c8b279a03
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-03-26 16:23:55 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 1.41 ms, Memory: 76.3 MB -BaekjoonHub

--- src/programmers/lv1/lv1_12903/Solution.java
+++ src/programmers/lv1/lv1_12903/Solution.java
@@ -3,18 +3,8 @@ package programmers.lv1.lv1_12903;
 class Solution {
     
     public String solution(String s) {
-        
-        String answer = "";
         String[] str = s.split("");
-        int mid = s.length()/2;
-            
-        if(s.length()%2 == 0){
-            answer = str[mid-1]+str[mid];
-        }else{
-            answer = str[mid];
-        }
-               
-        return answer;
+        return s.length()%2 == 0 ? str[s.length()/2-1]+ str[s.length()/2] : str[s.length()/2];
     }
     
 }
