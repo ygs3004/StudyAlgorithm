91954d3b259697da8310d9cf244b604f89d2cc49
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-03-29 00:36:17 +0900

    [level 1] Title: 폰켓몬, Time: 5.30 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ src/programmers/lv1/lv1_1845/Solution.java
@@ -0,0 +1,24 @@
+package programmers.lv1.lv1_1845;
+
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int[] nums) {
+        
+        int[] distinctArr = Arrays.stream(nums).distinct().toArray();
+        int a = distinctArr.length;
+        int answer = 1;
+
+        System.out.println(nums.length/2);
+        
+        if(nums.length/2 >= a){
+            answer = a;
+        }else{
+            answer = nums.length/2;
+        }
+       
+        return answer;
+    }
+    
+}
