ff339c901d05ebd3712dcb2f67480bd07c5498cc
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-03-29 08:55:27 +0900

    [level 1] Title: 모의고사, Time: 0.54 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ src/programmers/lv1/lv1_42840/Solution.java
@@ -0,0 +1,39 @@
+package programmers.lv1.lv1_42840;
+
+class Solution {
+    
+    public int[] solution(int[] answers) {
+        
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == arr1[i%5]) count1++;
+            if(answers[i] == arr2[i%8]) count2++;
+            if(answers[i] == arr3[i%10]) count3++;
+        }
+        
+        int[] result = {};
+        
+        if(count1 == count2 && count2 == count3){
+            return new int[]{1,2,3};  
+        } else if(count1 > count2 & count1 > count3){
+            return new int[]{1};  
+        } else if(count2 > count1 & count2 > count3){
+            return new int[]{2};  
+        } else if(count3 > count1 & count3 > count2){
+            return new int[]{3};  
+        } else if(count1 == count2){
+            return new int[]{1,2};  
+        } else if(count1 == count3){
+            return new int[]{1,3};
+        }else{
+            return new int[]{2,3};
+        }
+    }
+    
+}
