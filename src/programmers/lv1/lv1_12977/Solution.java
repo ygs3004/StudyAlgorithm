4dd66aa1ba04449beceb2f1c33e534e38745e0a6
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-03-30 12:50:01 +0900

    [level 1] Title: 소수 만들기, Time: 0.38 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ src/programmers/lv1/lv1_12977/Solution.java
@@ -0,0 +1,39 @@
+package programmers.lv1.lv1_12977;
+
+import java.util.*;
+
+class Solution {
+    
+    public int solution(int[] nums) {
+        
+        Arrays.sort(nums);
+        int max = nums[nums.length-1]+nums[nums.length-2]+nums[nums.length-3];
+        boolean[] decimal = new boolean[max+1];
+        
+        for(int i= 2; i<max+1; i++){
+            if(decimal[i]) continue;
+            for(int j=i*2; j<max+1; j+=i){
+                decimal[j] = true;
+            }
+        }
+        
+        int count = 0;
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i=0; i<nums.length-2; i++){
+            for(int j=i+1; j<nums.length-1; j++){
+                for(int k=j+1; k<nums.length; k++){
+                    list.add(nums[i]+nums[j]+nums[k]);
+                }
+            }
+        }
+        
+        for(int a : list){
+            if(!decimal[a]) count ++;
+        }
+
+        return count;
+    }
+    
+}
