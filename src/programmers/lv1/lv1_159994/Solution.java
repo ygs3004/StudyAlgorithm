f02e7c1d25e975cecb322301c745c04492d7a5c3
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-04-04 00:09:00 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 75.3 MB -BaekjoonHub

--- src/programmers/lv1/lv1_159994/Solution.java
+++ src/programmers/lv1/lv1_159994/Solution.java
@@ -2,19 +2,15 @@ package programmers.lv1.lv1_159994;
 
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
-        
         int idx1 = 0;
         int idx2 = 0;
-        
         for(int i=0; i<goal.length; i++){
             if(goal[i].equals(cards1[idx1])) idx1 = idx1 < cards1.length-1 ? idx1+1 : idx1;
             else if(goal[i].equals(cards2[idx2]))idx2 = idx2 < cards2.length-1 ? idx2+1 : idx2;
             else {
                 return "No";
             }
-
         }
         return "Yes";
     }
-    
 }
