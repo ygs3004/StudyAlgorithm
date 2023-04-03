5a51ff18f0e5733cd8363e9b2e29442e46188c46
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-04-03 23:14:29 +0900

    [level 1] Title: 덧칠하기, Time: 3.30 ms, Memory: 79.6 MB -BaekjoonHub

---
+++ src/programmers/lv1/lv1_161989/Solution.java
@@ -0,0 +1,35 @@
+package programmers.lv1.lv1_161989;
+
+import java.util.*;
+class Solution {
+    
+    public int solution(int n, int m, int[] section) {
+        
+        boolean[] wall = new boolean[section[section.length-1]+1];
+        
+        for(int block : section){
+            wall[block] = true;
+        }
+        
+        int start = section[0];
+        int answer = 0;
+        
+        while(wall[wall.length-1]){
+            for(int i = start; i<start+m; i++){
+                wall[i] = false;
+                if(i == wall.length-1) break;
+            }
+            answer++;
+            if(start < wall.length-1){
+                while(!wall[start]){
+                    start++;    
+                    if(start == wall.length-1) break;
+                }                
+            }
+
+        }
+        
+        return answer;
+    }
+    
+}
