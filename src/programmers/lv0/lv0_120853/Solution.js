a431f120f5664879330840ede81596b6f7432ceb
윤건수 <102002366+ygs3004@users.noreply.github.com>
2023-05-15 22:09:05 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- src/programmers/lv0/lv0_120853/Solution.js
+++ src/programmers/lv0/lv0_120853/Solution.js
@@ -1,5 +1,10 @@
 function solution(s) {
     let answer = 0;
-    s.split(" ").forEach((v, i, arr) =>  !isNaN(v) ? answer += +v : answer -= arr[i-1]);
+    // s.split(" ").forEach((v, i, arr) =>  !isNaN(v) ? answer += +v : answer -= arr[i-1]);
+    s.split(' ').reduce((a,c)=> {
+        answer = c == 'Z' ? answer - Number(a) : answer + Number(c);
+        return c
+    }, 0)
     return answer;
 }
+
