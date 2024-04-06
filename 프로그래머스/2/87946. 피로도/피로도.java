ecb16d3810d89a45652de327da75292cb88d34e0
윤건수 <ygs3004@naver.com>
2024-04-06 12:10:47 +0900

    [level 2] Title: 피로도, Time: 11.82 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Solution {
+    
+    int maxCount = 0;
+    List<Dungeon> dungeonList = new ArrayList();
+    
+    public int solution(int totalEnergy, int[][] dungeons) {
+        for(int[] dungeon: dungeons){
+            dungeonList.add(new Dungeon(dungeon[0], dungeon[1]));
+        }
+       
+        for(int i=0; i < dungeonList.size(); i++){
+            boolean[] visited = new boolean[dungeonList.size()];
+            searchDungeon(i, 0, totalEnergy, visited, "");
+        }
+        
+        return maxCount;
+    }
+   
+    private void searchDungeon(int index, int count, int totalEnergy, boolean[] visited, String list){
+        if(visited[index]) return;
+        
+        visited[index] = true;
+        if(dungeonList.get(index).minEnergy > totalEnergy) return;
+            
+        count++;
+        totalEnergy -= dungeonList.get(index).useEnergy;
+        maxCount = Math.max(count, maxCount);
+        list += " " + index;
+
+        for(int i=0; i<dungeonList.size(); i++){
+            if(visited[i] == false){
+                searchDungeon(i, count, totalEnergy, visited, list);   
+                visited[i] = false;    
+            }
+        }
+    }
+    
+        
+    private class Dungeon{
+        int minEnergy;
+        int useEnergy;
+        
+        Dungeon(int minEnergy, int useEnergy){
+            this.minEnergy = minEnergy;
+            this.useEnergy = useEnergy;
+        }
+    }
+    
+}
