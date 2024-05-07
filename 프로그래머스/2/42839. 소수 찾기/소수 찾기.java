1ec3d551faf5d76c7cbb02179a5b8251aa241e4f
윤건수 <ygs3004@naver.com>
2024-05-07 18:24:09 +0900

    [level 2] Title: 소수 찾기, Time: 91.38 ms, Memory: 94.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+class Solution {
+    
+    boolean[] prime = new boolean[10000000];
+    boolean[] checkResult = new boolean[10000000];
+    String[] number;
+    int answer = 0;
+    
+    public int solution(String numbers) {
+        
+        // 에라스토테네스의체 초기화
+        for(int i = 2; i<prime.length; i++){
+            prime[i] = true;
+        }
+        
+        for(int i=2; i<prime.length; i++){
+            if(prime[i] == false) continue;
+            for(int j=i*2; j<prime.length; j+=i){
+                prime[j] = false;
+            }
+        }
+        
+        number = numbers.split("");
+        for(int i=0; i<number.length; i++){
+            boolean visited[] = new boolean[number.length];
+            visited[i] = true;
+            
+            int makeNum = Integer.parseInt(number[i]);
+            checkPrime(makeNum);
+            dfs(makeNum, visited);
+        }
+        
+        return answer;
+    }
+    
+    private void dfs(int beforeMakeNum, boolean[] visited){
+        for(int i=0; i<visited.length; i++){
+            if(visited[i] == false){
+                visited[i] = true;
+                String beforeMakeNumStr = String.valueOf(beforeMakeNum);
+                String targetNumStr = number[i];
+                int makeNum =  Integer.parseInt(beforeMakeNumStr + targetNumStr);
+                checkPrime(makeNum);
+                dfs(makeNum, visited);
+                visited[i] = false;
+            }
+        }
+    }
+            
+    private void checkPrime(int n){
+        if(prime[n] == true && checkResult[n] == false) {
+            checkResult[n] = true;
+            answer++;
+        }
+    }
+}
