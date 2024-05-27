828e6691b38767cfae3afb8cdaf06c03df605d30
윤건수 <ygs3004@naver.com>
2024-05-27 23:57:56 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.66 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,49 +1,49 @@
 import java.util.*;
 
-class Solution {
+public class Solution {
     public int solution(int n, int[][] wires) {
         int totalNetwork = n;
         int answer = Integer.MAX_VALUE;
-        
+
         for(int i = 0; i < wires.length; i++){
-            // 연결이 끊긴 한곳만 체크하면 나머지도 알수있음
+            // 연결이 끊긴 한곳만 체크하면 나머지도 알수있 음
             int disconnectNetwork = wires[i][0];
             ArrayList[] connectInfo = createConnectInfo(i, n, wires);
             boolean[] visited = new boolean[n + 1];
             int linkCount = 0;
-            
+
             Queue<Integer> que = new LinkedList<>();
             que.add(disconnectNetwork);
+
             while(!que.isEmpty()){
                 int targetNetwork = que.poll();
                 linkCount++;
                 visited[targetNetwork] = true;
                 ArrayList<Integer> targetConnect = connectInfo[targetNetwork];
                 for(Integer connectNetwork : targetConnect){
-                    if(visited[connectNetwork]){
-                        continue;
-                    }else{
+                    if(!visited[connectNetwork]){
                         que.add(connectNetwork);
                     }
                 }
             }
+
             int anotherLinkCount = (totalNetwork - linkCount);
             int diff = linkCount > anotherLinkCount
-                ? linkCount - anotherLinkCount
-                : anotherLinkCount - linkCount;
+                    ? linkCount - anotherLinkCount
+                    : anotherLinkCount - linkCount;
 
             answer = Math.min(answer, diff);
         }
-        
+
         return answer;
     }
-    
+
     private ArrayList<Integer>[] createConnectInfo(int disconnectIdx, int n, int[][] wires){
-       // 0 번인덱스 미사용        
+        // 0 번인덱스 미사용
         ArrayList<Integer>[] connect = new ArrayList[n+1];
         for(int i = 1; i < n + 1; i++){
             connect[i] = new ArrayList<Integer>();
-        } 
+        }
         for(int i = 0; i < wires.length; i++){
             if(i == disconnectIdx) continue;
             int network1 = wires[i][0];
@@ -51,7 +51,7 @@ class Solution {
             connect[network1].add(network2);
             connect[network2].add(network1);
         }
-        
+
         return connect;
     }
-} 
+}
