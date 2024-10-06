3e6a08c5f63f1639479005559c09073a73c1de3f
윤건수 <ygs3004@naver.com>
2024-10-06 23:59:44 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 1;
+        while(n>1){
+          if(n%2 != 0) ans++;
+          n = n%2 == 0 ? n/2 : n-1;   
+        }   
+        return ans;
+    }
+}
