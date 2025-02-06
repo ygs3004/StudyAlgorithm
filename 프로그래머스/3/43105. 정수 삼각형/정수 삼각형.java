85285a969c36910cb2d2a0941805cc1dbfed7fbf
윤건수 <ygs3004@naver.com>
2025-02-07 00:15:40 +0900

    [level 3] Title: 정수 삼각형, Time: 33.37 ms, Memory: 66.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] triangle) {
+        List<Integer>[] dp = new List[triangle.length];
+        
+        for(int i = 0; i < triangle.length; i++){
+            dp[i] = new ArrayList<Integer>();
+        }
+        
+        dp[0].add(triangle[0][0]);
+        
+        for(int i = 1; i < triangle.length; i++){
+            List<Integer> curFloor = dp[i];
+            List<Integer> beforeFloor = dp[i - 1];
+            
+            for(int j = 0; j < triangle[i].length; j++){
+                int bestSum = 0;
+                if(j == 0){
+                    bestSum = triangle[i][j] + beforeFloor.get(j);
+                }else if(j == triangle[i].length - 1){
+                    bestSum = triangle[i][j] + beforeFloor.get(j - 1);
+                }else{
+                    bestSum = triangle[i][j] + Math.max(beforeFloor.get(j), beforeFloor.get(j - 1));
+                }
+                curFloor.add(bestSum);
+            }
+        }
+        
+        int answer = 0;
+        List<Integer> lastFloor = dp[triangle.length - 1];
+        for(int sumResult : lastFloor){
+            answer = Math.max(answer, sumResult);
+        }
+        
+        return answer;
+    }
+}
