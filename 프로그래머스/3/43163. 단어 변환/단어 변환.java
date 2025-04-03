6ef075edc7ed16a7a60ec76099092ae0f8260e75
윤건수 <ygs3004@naver.com>
2025-04-04 03:07:11 +0900

    [level 3] Title: 단어 변환, Time: 0.40 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,18 +2,22 @@ import java.util.*;
 
 class Solution {
     
-    static int answer;
-    static List[] graphs;
+    int answer;
+    List[] graphs;
     boolean[] visited; 
+    String[] words;
+    String target;
     
     public int solution(String begin, String target, String[] words) {
-        visited = new boolean[words.length];
-        graphs = new ArrayList[words.length];   
+        this.words = words;
+        this.target = target;
+        this.visited = new boolean[words.length];
+        this.answer = Integer.MAX_VALUE;
+        this.graphs = new ArrayList[words.length];   
+        
         for(int i = 0; i < graphs.length; i++){
             graphs[i] = new ArrayList<>();
         }
-        
-        answer = Integer.MAX_VALUE;
         
         for(int i = 0; i < words.length - 1; i++){
             String str1 = words[i];
@@ -29,20 +33,18 @@ class Solution {
         for(int i = 0; i < words.length; i++){
             String word = words[i];
             if(canChange(begin, word)){
-                System.out.println("==============");
-                dfs(words, i, target, 1);
+                dfs(i, 1);
             }
         }
         
         return answer == Integer.MAX_VALUE ? 0 : answer;
     }
     
-    private void dfs(String[] words, int curIdx, String target, int depth){
+    private void dfs(int curIdx, int depth){
         if(visited[curIdx]) return;
         visited[curIdx] = true;
         
         String word = words[curIdx];
-        System.out.println(word + " " + depth);
         if(word.equals(target)){
             answer = Math.min(depth, answer);
             return;
@@ -51,7 +53,7 @@ class Solution {
         List<Integer> graph = graphs[curIdx];
         for(int nextIdx : graph){
             if(!visited[nextIdx]){
-                dfs(words, nextIdx, target, depth + 1);
+                dfs(nextIdx, depth + 1);
                 visited[nextIdx] = false;   
             }
         }
