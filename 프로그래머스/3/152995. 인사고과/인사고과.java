93a69ded06519bfa4960bfd68241a1b119197bac
윤건수 <ygs3004@naver.com>
2026-09-22 00:11:15 +0900

    [level 3] Title: 인사고과, Time: 135.49 ms, Memory: 131 MB -BaekjoonHub

--- 프로그래머스/3/152995. 인사고과/인사고과.java
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -2,86 +2,60 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] scores) {
-
-        int n = scores.length;
-
-        int[][] arr = new int[n][3];
-
-        for (int i = 0; i < n; i++) {
-            arr[i][0] = scores[i][0];
-            arr[i][1] = scores[i][1];
-            arr[i][2] = i;
-        }
-
-        Arrays.sort(arr, (a, b) -> {
-            if (a[0] == b[0]) {
-                return b[1] - a[1];
+        int baseScore = scores[0][0] + scores[0][1];
+        
+        PriorityQueue<int[]> attitudePq = new PriorityQueue<>((a, b) -> {
+            if(b[0] == a[0] && b[1] == a[1]){
+                return b[2] - a[2];
+            }else if(b[0] == a[0]){
+                return a[1] - b[1];
             }
             return b[0] - a[0];
         });
-
-        int maxWorkerScore = -1;
-        List<int[]> valid = new ArrayList<>();
-
-        int i = 0;
-
-        while (i < n) {
-
-            int attitude = arr[i][0];
-
-            // 같은 태도 점수 그룹
-            int j = i;
-
-            while (j < n && arr[j][0] == attitude) {
-                j++;
+        PriorityQueue<int[]> workerPq = new PriorityQueue<>((a, b) -> {
+            if(b[0] == a[0] && b[1] == a[1]){
+                return b[2] - a[2];
+            }else if(b[1] == a[1]){
+                return b[0] - a[0];
             }
-
-            // 같은 그룹 내부에서는
-            // 이전 그룹의 maxWorkerScore만 가지고 판단
-            for (int k = i; k < j; k++) {
-
-                int[] score = arr[k];
-
-                if (score[1] < maxWorkerScore) {
-                    continue;
+            return a[1] - b[1];
+        });
+        List<int[]> total = new ArrayList<>();
+        
+        for(int i = 0; i < scores.length; i++){
+            int[] score = scores[i];
+            attitudePq.add(new int[]{score[0], score[1], i});
+            workerPq.add(new int[]{score[0], score[1], i});
+        }
+        
+        boolean[] bad = new boolean[scores.length];
+        while(!attitudePq.isEmpty()){
+            int[] score = attitudePq.poll();
+            int id = score[2];
+            if(bad[id]) continue;
+            
+            List<int[]> save = new ArrayList<>();
+            while(!workerPq.isEmpty()){
+                int[] check = workerPq.poll();
+                int checkId = check[2];
+                if(checkId == id){
+                    break;
+                }else if(check[1] >= score[1] || check[0] >= score[0]){
+                    save.add(check);
+                }else{
+                    bad[checkId] = true;    
                 }
-
-                valid.add(score);
             }
-
-            // 그룹을 전부 판단한 뒤 max 갱신
-            for (int k = i; k < j; k++) {
-                maxWorkerScore =
-                    Math.max(maxWorkerScore, arr[k][1]);
+            
+            for(int[] saveScore: save){
+                workerPq.add(saveScore);
             }
-
-            i = j;
+            
+            if(score[0] + score[1] > baseScore){
+                total.add(score);    
+            }
         }
-
-        valid.sort((a, b) ->
-            (b[0] + b[1]) - (a[0] + a[1])
-        );
-
-        int rank = 0;
-        int beforeScore = -1;
-
-        for (int k = 0; k < valid.size(); k++) {
-
-            int[] score = valid.get(k);
-
-            int total = score[0] + score[1];
-
-            if (total != beforeScore) {
-                rank = k + 1;
-            }
-
-            if (score[2] == 0) {
-                return rank;
-            }
-
-            beforeScore = total;
-        }
-
-        return -1;
+        
+        return bad[0] ? -1 : total.size() + 1;
     }
 }
